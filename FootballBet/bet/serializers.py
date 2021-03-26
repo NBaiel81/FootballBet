cbfce3905528062fd90a4c608cd0b9331995de64
NBaiel81 <nurmatbekov0771503907@gmail.com>
2021-03-26 17:15:47 +0600

    hello

--- FootballBet/bet/serializers.py
+++ FootballBet/bet/serializers.py
@@ -9,16 +9,26 @@ class BetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bet
         fields = ['betuser','bet_amount','date_created','match','result','bet_money']
+
     def get_bet_money(self,obj):
-        bet_money=obj.bet_amount
-        bets = Bet.objects.all()
-        for bet in bets:
-            if bet.betuser == obj.betuser and bet.result != obj.result:
-                if obj.betuser.wallet >= bet_money:
-                    obj.betuser.wallet -= bet_money
-                    obj.betuser.save()
-
-
-                else:
-                    obj.delete()
-                    raise ValidationError("Not enough money!")
+        bet_money = obj.bet_amount
+        if obj.match.match_status == "haven't started":
+            if obj.betuser.wallet >= bet_money:
+                obj.betuser.wallet -= bet_money
+                obj.betuser.save()
+            else:
+                obj.delete()
+                raise ValidationError("Not enough money ")
+        else:
+            obj.delete()
+            raise ValidationError("Time is up")
+        # bets = Bet.objects.all()
+        # for bet in bets:
+        #     if bet.betuser == obj.betuser and bet.result != obj.result and obj.match.match_status == "haven't started":
+        #         print(1)
+        #         if obj.betuser.wallet >= bet_money:
+        #             obj.betuser.wallet -= bet_money
+        #             obj.betuser.save()
+        #         else:
+        #             obj.delete()
+        #             raise ValidationError("Not enough money!")
--- FootballBet/match/serializers.py
+++ FootballBet/match/serializers.py
@@ -9,10 +9,16 @@ class MatchSerializer(serializers.ModelSerializer):
     def get_match(self,obj):
         if obj.match_status =='finished':
             bets=obj.bet_set.all()
+            coef_A = obj.bet_set.filter(result='TeamA won').count()
+            coef_B = obj.bet_set.filter(result='TeamB won').count()
+
             for bet in bets:
                 print(obj.result)
-                if bet.result == obj.result:
-                    bet.betuser.wallet += bet.bet_amount * 2
+                if bet.result == 'TeamA won' and bet.result == obj.result:
+                    bet.betuser.wallet += (bet.bet_amount * (coef_B/coef_A)) +bet.bet_amount
+                    bet.betuser.save()
+                elif bet.result == 'TeamB won' and bet.result == obj.result:
+                    bet.betuser.wallet += bet.bet_amount * (coef_A / coef_B) +bet.bet_amount
                     bet.betuser.save()
 
 
