59cf08bb4ed4af0af564e771c27448a191ee0114
NBaiel81 <nurmatbekov0771503907@gmail.com>
2021-03-29 17:23:08 +0600

    hello

--- FootballBet/FootballBet/settings.py
+++ FootballBet/FootballBet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-kijzdvcez)p(g*mwi2prs$b=wti8i3!-zg=ht&e#@%bic&c8_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- FootballBet/match/serializers.py
+++ FootballBet/match/serializers.py
@@ -5,7 +5,7 @@ class MatchSerializer(serializers.ModelSerializer):
     match=serializers.SerializerMethodField()
     class Meta:
         model = Match
-        fields = ['teamA','teamB','match_status','result','match']
+        fields = ['teamA','teamB','match_status','result','match','id']
     def get_match(self,obj):
         if obj.match_status =='finished':
             bets=obj.bet_set.all()
@@ -14,12 +14,20 @@ class MatchSerializer(serializers.ModelSerializer):
 
             for bet in bets:
                 print(obj.result)
-                if bet.result == 'TeamA won' and bet.result == obj.result:
-                    bet.betuser.wallet += (bet.bet_amount * (coef_B/coef_A)) +bet.bet_amount
-                    bet.betuser.save()
-                elif bet.result == 'TeamB won' and bet.result == obj.result:
-                    bet.betuser.wallet += bet.bet_amount * (coef_A / coef_B) +bet.bet_amount
-                    bet.betuser.save()
+                if coef_A != 0 or coef_B != 0:
+                    if bet.result == 'TeamA won' and bet.result == obj.result:
+                        bet.betuser.wallet += (bet.bet_amount * 2) +bet.bet_amount
+                        bet.betuser.save()
+                    elif bet.result == 'TeamB won' and bet.result == obj.result:
+                        bet.betuser.wallet += (bet.bet_amount * 2) +bet.bet_amount
+                        bet.betuser.save()
+                else:
+                    if bet.result == 'TeamA won' and bet.result == obj.result:
+                        bet.betuser.wallet += (bet.bet_amount * (coef_B/coef_A)) +bet.bet_amount
+                        bet.betuser.save()
+                    elif bet.result == 'TeamB won' and bet.result == obj.result:
+                        bet.betuser.wallet += bet.bet_amount * (coef_A / coef_B) +bet.bet_amount
+                        bet.betuser.save()
 
 class MatchUpdateSerializer(serializers.Serializer):
     statuses = (
